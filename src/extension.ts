3b502b3ed25a235919405affb9b667df986e9fb2
Lengyue57 <61723363+Lengyue57@users.noreply.github.com>
2024-07-02 16:26:18 +0800

    fix: The internal text deleted when use "${0}"   instead of "$0" for snippet string in the single-line selection

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,9 @@
 'use strict';
 import * as vscode from 'vscode';
+import {
+  SnippetString,
+  SnippetTextEdit,
+} from 'vscode';
 
 const messages = {
   unselection: "Currently not selected",
@@ -127,66 +131,66 @@ function WrapSelections(
   snippet_str: string,
 ) {
   const { document, selections } = editor;
-  const snippet_edits = [] as vscode.SnippetTextEdit[];
+  const snippet_edits = [] as SnippetTextEdit[];
 
   for (const selection of selections) {
     if (selection.isEmpty)
       continue;
 
     let wrap_range = selection as vscode.Range;
-    let actual_snippet_str = snippet_str;
+    let actual_snippet_str = snippet_str.replace(match_$0, "\$0");
     let inner_text = document.getText(wrap_range).replace(/\\/g, "\\\\")
       .replace(/([$}])/g, "\\$1");
 
     if (selection.isSingleLine) {
-      const snippet = new vscode.SnippetString(
+      const snippet = new SnippetString(
         actual_snippet_str.replace("\$0", `\${0:${inner_text}}`)
       );
 
-      snippet_edits.push(new vscode.SnippetTextEdit(wrap_range, snippet));
+      snippet_edits.push(new SnippetTextEdit(wrap_range, snippet));
 
       continue;
     }
 
 
-      const start_line = document.lineAt(selection.start);
+    const start_line = document.lineAt(selection.start);
 
-      if (selection.start.character === start_line.text.length) {
-        // 光标在行尾
-        // The cursor is at the end of the line
+    if (selection.start.character === start_line.text.length) {
+      // 光标在行尾
+      // The cursor is at the end of the line
 
-        const wrap_start_line = document.lineAt(selection.start.line + 1);
+      const wrap_start_line = document.lineAt(selection.start.line + 1);
 
-        wrap_range = new vscode.Range(
+      wrap_range = new vscode.Range(
         wrap_start_line.lineNumber,
         wrap_start_line.firstNonWhitespaceCharacterIndex,
         selection.end.line,
         selection.end.character
-        );
+      );
     } else if (
       selection.start.character < start_line.firstNonWhitespaceCharacterIndex
     ) {
-        // 光标在行首
-        // The cursor is at the beginning of the line
+      // 光标在行首
+      // The cursor is at the beginning of the line
 
-        wrap_range = new vscode.Range(
+      wrap_range = new vscode.Range(
         start_line.lineNumber,
         start_line.firstNonWhitespaceCharacterIndex,
         selection.end.line,
         selection.end.character
-        );
-      }
+      );
+    }
 
     inner_text = document.getText(wrap_range).split(/\n\r?/)
       .reduce((prev_line, curr_line) => prev_line + "\n\t" + curr_line);
 
-    actual_snippet_str = snippet_str.replace(match_$0, "\n\t\$0\n");
+    actual_snippet_str = snippet_str.replace("\$0", "\n\t\$0\n");
 
-    const snippet = new vscode.SnippetString(
+    const snippet = new SnippetString(
       actual_snippet_str.replace("\$0", `\${0:${inner_text}}`)
     );
 
-    snippet_edits.push(new vscode.SnippetTextEdit(wrap_range, snippet));
+    snippet_edits.push(new SnippetTextEdit(wrap_range, snippet));
   }
 
 
